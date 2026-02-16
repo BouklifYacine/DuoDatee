cd7763f390cdac3d1cfd5ac519eaa61108fc7bff
Yacine Bouklif <yacine69800@live.fr>
2026-02-16 20:10:06 +0100

    fix: fix some unused code

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -2,14 +2,9 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import "react-native-reanimated";
 
 import { useColorScheme } from "@/hooks/use-color-scheme";
 import { queryClient } from "@/lib/query-client";
-
-export const unstable_settings = {
-  anchor: "(tabs)",
-};
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
@@ -20,7 +15,6 @@ export default function RootLayout() {
         <Stack initialRouteName="(auth)">
           <Stack.Screen name="(auth)" options={{ headerShown: false }} />
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="modal" options={{ presentation: "modal", title: "Modal" }} />
         </Stack>
         <StatusBar style="auto" />
       </ThemeProvider>
