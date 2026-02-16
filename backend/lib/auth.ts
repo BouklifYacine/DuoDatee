9545d3ca377ceb08dfdf1093f036819c1ed43b63
Yacine Bouklif <yacine69800@live.fr>
2026-02-16 15:12:21 +0100

    setup hono for better auth and email password login

--- backend/lib/auth.ts
+++ backend/lib/auth.ts
@@ -5,4 +5,21 @@ export const auth = betterAuth({
     database: prismaAdapter(prisma, {
         provider: "postgresql",
     }),
+
+    emailAndPassword: {
+        enabled: true,
+        autoSignIn: false
+    },
+
+    advanced: {
+        defaultCookieAttributes: {
+            sameSite: "none",
+            secure: true,
+            partitioned: true // New browser standards will mandate this for foreign cookies
+        },
+        crossSubDomainCookies: {
+            enabled: true
+        }
+    }
+
 });
